369c3bb438bd8626de3b3343b93f2abf1b0ab6f1
Sreedeep Pradeep <pradeepantkmalappattam@gmail.com>
2026-08-09 09:25:07 +0530

    Implement basic TCP client functionality

---
+++ Computer_Network/socket_programming/TCP/client.py
@@ -0,0 +1,22 @@
+import socket
+
+c = socket.socket()
+print('client socket has been created')
+
+SERVER_IP = 'localhost'
+SERVER_PORT = 5000
+c.connect((SERVER_IP , SERVER_PORT))
+
+while True:
+  RECIVED_MESSAGE = c.recv(1024).decode('utf-8')
+  print('SERVER : ' , RECIVED_MESSAGE )
+
+  if(RECIVED_MESSAGE == 'exiting...'):
+    break:
+
+  MESSAGE_SEND = input('YOUR MESSAGE : ')
+  c.send(bytes(MESSAGE_SEND,'utf-8'))
+
+c.close()
+print('client stoped')
+  
